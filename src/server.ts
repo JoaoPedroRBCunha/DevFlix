c581af1f38aaab39e44ba6f31e69d82a752f6330
Raul Pontes <raulluar11222@gmail.com>
2024-10-24 16:27:11 -0300

    Relacionamento entre modelos

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,13 @@
 import express from "express";
 import { sequelize } from "./database";
+import { adminJsInst, adminJsRouter } from "./adminjs";
 
 const app = express();
+
+app.use(express.static("public"));
+
+app.use(adminJsInst.options.rootPath, adminJsRouter);
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
